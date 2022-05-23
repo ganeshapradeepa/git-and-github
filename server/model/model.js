dc4a496d57b2bb7ef2625733cc7593791c481a49
rakesh <rocky15012001v@gmail.com>
2022-05-23 12:00:07 +0530

    Intial commit

---
+++ server/model/model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+var validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+}
+
+var schema = new mongoose.Schema({
+    name : {
+        type : String,
+        required: true
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: 'Email address is required',
+      validate: [validateEmail, 'Please fill a valid email address'],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+  },
+
+    gender : String,
+    status : String
+})
+
+
+const Userdb = mongoose.model('userdb', schema);
+
+module.exports = Userdb;
